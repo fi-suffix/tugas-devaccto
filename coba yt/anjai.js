673b2cd823668cfd535627b3a401f336de74255f
fi-suffix <fadildafiamsyah@gmail.com>
2025-10-02 09:36:22 +0700

    buat tugas

---
+++ coba yt/anjai.js
@@ -0,0 +1,10 @@
+// Smooth scroll untuk navigasi
+document.querySelectorAll('nav a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
